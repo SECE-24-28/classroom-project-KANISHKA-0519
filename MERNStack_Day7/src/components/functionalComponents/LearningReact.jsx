b006d9b178abc6c4cfc666a3bd49f621c7ab7234
KANISHKA V S <kanishkasekar20@gmail.com>
2025-12-11 18:20:52 +0530

    Add files via upload

---
+++ MERNStack_Day7/src/components/functionalComponents/LearningReact.jsx
@@ -0,0 +1,11 @@
+const LearningReact = () => {
+  return (
+    <div>
+      <h1>Learning React</h1>
+      <p>This component is for learning React functional components.
+      </p>
+    </div>
+  );
+};
+
+export default LearningReact;
